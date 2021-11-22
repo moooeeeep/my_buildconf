ac821875643345113ff31045fac581d3538c054c
moooeeeep <moooeeeep@users.noreply.github.com>
2021-11-22 12:22:45 +0100

    Update init.rb
    want to use python3

--- init.rb
+++ init.rb
@@ -3,3 +3,4 @@ require 'autoproj/git_server_configuration'
 Autoproj.git_server_configuration('GITHUB', 'github.com', default: 'http,http', :http_url => 'https://github.com')
 
 Autoproj.config.set "USE_PYTHON", true
+Autoproj.config.set "python_executable", "/usr/bin/python3"
